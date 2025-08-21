108c519ebd25d3823db00a2c2b2e9354c377f47e
tushar10-web <tusharr.8487@gmail.com>
2025-08-21 11:55:33 +0530

    Delete fynix directory

--- fynix/backend/test-db.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('./db');
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
