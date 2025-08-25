b363d047b06518bb8f932fed92437e8150a73f21
tushar10-web <tusharr.8487@gmail.com>
2025-08-25 18:50:06 +0530

    Add files via upload

---
+++ backend/models/index.js
@@ -0,0 +1,5 @@
+const Wallet = require("./Wallet");
+const Transaction = require("./Transaction");
+const User = require("./User");  // Add this line to import User
+
+module.exports = { Wallet, Transaction, User };  // Export User as well
